91bf381208dedab3e4ad47f8994a4179486e85ec
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-14 18:18:33 +0100

    Add TemplatesModule with TemplateFullWidthComponent

---
+++ src/app/templates/templates.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TemplateFullWidthComponent } from './template-full-width/template-full-width.component';
+
+
+
+@NgModule({
+  declarations: [
+    TemplateFullWidthComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TemplatesModule { }
