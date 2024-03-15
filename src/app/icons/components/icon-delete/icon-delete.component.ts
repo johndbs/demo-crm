02427a822bb7cc49e4a13f941aa2b9d2022607c6
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-15 15:36:35 +0100

    Add icon components and styles

---
+++ src/app/icons/components/icon-delete/icon-delete.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-icon-delete',
+  templateUrl: './icon-delete.component.html',
+  styleUrl: './icon-delete.component.scss'
+})
+export class IconDeleteComponent {
+
+  public myIcon= faTrash;
+
+}
--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconCloseComponent } from './components/icon-close/icon-close.component';
+import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IconCloseComponent,
+    IconDeleteComponent,
+    IconEditComponent,
+    IconNavComponent
+  ],
   imports: [
     CommonModule,
     FontAwesomeModule
+  ],
+  exports: [ 
+    IconCloseComponent,
+    IconDeleteComponent,
+    IconEditComponent,
+    IconNavComponent
   ]
 })
 export class IconsModule { }
