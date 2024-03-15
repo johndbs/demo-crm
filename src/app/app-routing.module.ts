42413e39b5ee460dd3987853e8ffb49693302756
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-15 16:45:14 +0100

    Add orders module and related components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: '/sign-in', pathMatch: 'full'},
   {path: 'clients', loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)},
+  {path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)},
 ];
 
 @NgModule({
