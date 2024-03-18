f1fe7766a5aa56bdfb5bce4d90fc71e8730fffe1
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-18 16:18:40 +0100

    Refactor OrdersService to utilize environment variable for API URL
    - Update OrdersService to fetch API URL from environment configurations
    - Ensure dynamic API endpoint adjustment based on the environment

--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -1,28 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Order } from '../../core/models/order';
-import { Observable, of } from 'rxjs';
-import { StateOrder } from '../../core/enums/state-order';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environnement } from '../../../environnements/environnement';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
 
-  public collection: Order[] = [
-    {id: 1, dailyRateExVAT: 1200, numberOfDays: 20, vat: 20, state: StateOrder.PAID, serviceType: 'Dev Angular', client: 'WorldCompany', comment: 'Top comment'},
-    {id: 2, dailyRateExVAT: 1000, numberOfDays: 10, vat: 20, state: StateOrder.CANCELED, serviceType: 'Dev React', client: 'WorldCompany', comment: 'Project cancelled'}, 
-    {id: 3, dailyRateExVAT: 1000, numberOfDays: 5, vat: 20, state: StateOrder.OPTION, serviceType: 'Dev Vue', client: 'Mega Corp', comment: 'New prospect'},
-    {id: 4, dailyRateExVAT: 1000, numberOfDays: 60, vat: 20, state: StateOrder.CONFIRMED, serviceType: 'Dev Angular', client: 'Mega Corp', comment: 'New prospect, second project'},
-    {id: 5, dailyRateExVAT: 1000, numberOfDays: 200, vat: 20, state: StateOrder.OPTION, serviceType: 'Dev Angular', client: 'Tiny corp', comment: 'New prospect'},
-    {id: 6, dailyRateExVAT: 1500, numberOfDays: 20, vat: 20, state: StateOrder.OPTION, serviceType: 'Dev spring REST app', client: 'Tiny corp', comment: 'New prospect'}
-  ];
+  private apiUrl = environnement.apiUrl;
+
 
   constructor(private http: HttpClient) {}
 
 
   public getCollection(): Observable<Order[]> {
-    return this.http.get<Order[]>(`http://localhost:3000/orders`);
+    return this.http.get<Order[]>(`${this.apiUrl}/orders`);
   }
 
 
