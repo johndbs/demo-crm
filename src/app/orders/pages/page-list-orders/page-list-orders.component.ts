261ea377e2714e4becdd37d028eee6bcea28351f
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-18 14:33:49 +0100

    Reuse tablelight and btn components in page-list-orders
    - Implement tablelight component for order listings
    - Utilize btn component for action buttons

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Order } from '../../../core/models/order';
+import { StateOrder } from '../../../core/enums/state-order';
 
 @Component({
   selector: 'app-page-list-orders',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './page-list-orders.component.scss'
 })
 export class PageListOrdersComponent {
+
+  public collection: Order[] = [
+    {id: 1, dailyRateExVAT: 1200, numberOfDays: 20, vat: 20, state: StateOrder.PAID, serviceType: 'Dev Angular', client: 'WorldCompany', comment: 'Top comment'},
+    {id: 2, dailyRateExVAT: 1000, numberOfDays: 10, vat: 20, state: StateOrder.CANCELED, serviceType: 'Dev React', client: 'WorldCompany', comment: 'Project cancelled'}, 
+    {id: 3, dailyRateExVAT: 1000, numberOfDays: 5, vat: 20, state: StateOrder.OPTION, serviceType: 'Dev Vue', client: 'Mega Corp', comment: 'New prospect'},
+    {id: 4, dailyRateExVAT: 1000, numberOfDays: 60, vat: 20, state: StateOrder.CONFIRMED, serviceType: 'Dev Angular', client: 'Mega Corp', comment: 'New prospect, second project'},
+    {id: 5, dailyRateExVAT: 1000, numberOfDays: 200, vat: 20, state: StateOrder.OPTION, serviceType: 'Dev Angular', client: 'Tiny corp', comment: 'New prospect'}
+  ];
+
+
+  public headers: string[] = [
+    "ID",
+    "Client",
+    "Nb days",
+    "Daily rate",
+    "Total Ex VAT",
+    "Total Inc VAT",
+    "State"
+  ];
+
+  public total(val: number, coef: number, vat?: number) : number {
+    if(vat){
+      return val * coef * (1 + vat/100);
+    }
+    return val * coef;
+  }
+
+
 
 }
