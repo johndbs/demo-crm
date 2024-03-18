8876d1a5b8bcbf8bef321d9420b68818ecca534d
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-18 14:32:18 +0100

    Create tablelight and btn components in shared module
    - Add tablelight component for reusable table display
    - Add btn component for consitent button styling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplatesModule } from '../templates/templates.module';
 import { IconsModule } from '../icons/icons.module';
+import { BtnComponent } from './components/btn/btn.component';
+import { TableLightComponent } from './components/table-light/table-light.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BtnComponent,
+    TableLightComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     IconsModule,
-    TemplatesModule
+    TemplatesModule, 
+    BtnComponent,
+    TableLightComponent
   ]
 })
 export class SharedModule { }
