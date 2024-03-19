fec433b7eb717f37e620443d03cb81b518a4c1c2
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-19 17:15:49 +0100

    Refactor TemplateContainerComponent to utilize @Input for title
    - Modify TemplateContainerComponent to accept title via @Input decorator, enhancing component flexibility

--- src/app/templates/components/template-container/template-container.component.ts
+++ src/app/templates/components/template-container/template-container.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-template-container',
   templateUrl: './template-container.component.html',
   styleUrl: './template-container.component.scss'
 })
-export class TemplateContainerComponent implements OnInit{
+export class TemplateContainerComponent{
 
-  public title!: string;
-
-  ngOnInit(): void {
-    this.title = 'Change the template title here!';
-  }
+  @Input() title!: string;
 
 }
