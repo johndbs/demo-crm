82ebcc3b54c144dd05ae70bb110dcf1ad198d5b9
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-20 10:07:09 +0100

    Implement business logic for page-add-order and integrate form-order component
    - Develop the business logic for handling new order submission in page-add-order
    - Include form-order component within page-add-order

--- src/app/orders/pages/page-add-order/page-add-order.component.ts
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Order } from '../../../core/models/order';
+import { OrdersService } from '../../services/orders.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-add-order',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './page-add-order.component.scss'
 })
 export class PageAddOrderComponent {
+
+  public item: Order = new Order();
+
+  constructor(private ordersService: OrdersService, private router: Router) {}
+
+  public action(item: Order): void {
+    this.ordersService.add(item).subscribe((res) => {
+      this.router.navigate(['orders']);
+    }); 
+  }
 
 }
