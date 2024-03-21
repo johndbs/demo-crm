049ab761e7cf6bad7f1a124aa97546aeec3c2162
johndbs <83272366+johndbs@users.noreply.github.com>
2024-03-21 17:06:03 +0100

    Integrate dayjs library and display current date in footer
    - Add dayjs library for enhanced date manipulation and formatting capabilities
    - Implement display of the current year in the application's footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  public date = dayjs().format('YYYY');
 
 }
